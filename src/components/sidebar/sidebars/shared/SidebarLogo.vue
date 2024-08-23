b3f07d102b77880248cdf43c0c40ea36ec18b1ea
Marie-Eve Frechette <marieeve@rollin.org>
2024-08-23 10:43:39 -0400

    Fix quality logo

--- src/components/sidebar/sidebars/shared/SidebarLogo.vue
+++ src/components/sidebar/sidebars/shared/SidebarLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="sidebar-header flex gap-4 flex-col-reverse justify-between gap-x-4w w-full">
         <div class="sidebar-logo w-full flex items-center justify-center md:mb-2">
-            <img src="../../../../../images/logos/dsj_logo.png" alt="Logo" class="w-24 md:w-36 h-fit">
+            <img src="../../../../../images/logos/dsj_logo.png" alt="Logo">
         </div>
     </div>
 </template>
