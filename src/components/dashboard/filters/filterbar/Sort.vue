32ff542acdfc9d9dc60b37d455e139a03fef0b34
Marie-Eve Frechette <marieeve@rollin.org>
2024-08-28 13:38:27 -0400

    adjustment to the sorting component

--- src/components/dashboard/filters/filterbar/Sort.vue
+++ src/components/dashboard/filters/filterbar/Sort.vue
@@ -31,7 +31,7 @@ export default {
     data() {
         return {
             visibleRightSort: false,
-            selectedSortOrder: '',
+            selectedSortOrder: 'recentToOldest',
         };
     },
     methods: {
